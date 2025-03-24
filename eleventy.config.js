eb575bf9412efa067460b375962f162867e622f3
Robin Berjon <robin.berjon@nytimes.com>
2025-03-24 18:24:06 +0100

    ugly but running

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,6 @@
 
 import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
-import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 import pluginFilters from "./_config/filters.js";
@@ -38,7 +37,6 @@ export default async function (eleventyConfig) {
 	});
 
 	// Official plugins
-	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(HtmlBasePlugin);
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
@@ -46,12 +44,7 @@ export default async function (eleventyConfig) {
 		type: "atom", // or "rss", "json"
 		outputPath: "/feed.atom",
 		stylesheet: "pretty-atom-feed.xsl",
-		templateData: {
-			eleventyNavigation: {
-				key: "Feed",
-				order: 4,
-			},
-		},
+		templateData: {},
 		collection: {
 			name: "news",
 			limit: 10,
