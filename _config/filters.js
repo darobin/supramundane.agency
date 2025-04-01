fc43131277603e084a8af96eef8c51ab1420e7d7
Robin Berjon <robin.berjon@nytimes.com>
2025-04-01 12:30:08 +0200

    news

--- _config/filters.js
+++ _config/filters.js
@@ -2,34 +2,58 @@
 import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
-	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
-		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "yyyy-LL-dd");
-	});
+  eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+    // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+    return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "yyyy-LL-dd");
+  });
 
-	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat('yyyy-LL-dd');
-	});
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat('yyyy-LL-dd');
+  });
 
-	// Get the first `n` elements of a collection.
-	eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0)  return [];
-		if(n < 0) return array.slice(n);
-		return array.slice(0, n);
-	});
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if(!Array.isArray(array) || array.length === 0)  return [];
+    if(n < 0) return array.slice(n);
+    return array.slice(0, n);
+  });
 
-	// Return the smallest number argument
-	eleventyConfig.addFilter("min", (...numbers) => Math.min.apply(null, numbers));
+  // Get upcoming events
+  eleventyConfig.addFilter("upcoming", (array) => {
+    const today = DateTime.fromJSDate(new Date(), { zone: "utc" }).toFormat('yyyy-LL-dd');
+    return sortByDate(array, true).filter(obj => obj?.data?.date && DateTime.fromJSDate(obj?.data?.date, { zone: "utc" }).toFormat('yyyy-LL-dd') >= today);
+  });
+  eleventyConfig.addFilter("preceding", (array) => {
+    const today = DateTime.fromJSDate(new Date(), { zone: "utc" }).toFormat('yyyy-LL-dd');
+    return sortByDate(array).filter(obj => obj?.data?.date && DateTime.fromJSDate(obj?.data?.date, { zone: "utc" }).toFormat('yyyy-LL-dd') < today);
+  });
 
-	// Return the keys used in an object
-	eleventyConfig.addFilter("getKeys", target => Object.keys(target));
+  // Return the smallest number argument
+  eleventyConfig.addFilter("min", (...numbers) => Math.min.apply(null, numbers));
 
-	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "news"].indexOf(tag) === -1);
-	});
+  // Return the keys used in an object
+  eleventyConfig.addFilter("getKeys", target => Object.keys(target));
 
-	eleventyConfig.addFilter("sortAlphabetically", strings =>
-		(strings || []).sort((b, a) => b.localeCompare(a))
-	);
-};
+  eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "news"].indexOf(tag) === -1);
+  });
+
+  eleventyConfig.addFilter("sortAlphabetically", strings =>
+    (strings || []).sort((b, a) => b.localeCompare(a))
+  );
+}
+
+function sortByDate (array, ascending) {
+  const factor = ascending ? -1 : 1;
+  return (array || []).sort((a, b) => {
+    const at = a?.data?.date?.getTime();
+    const bt = b?.data?.date?.getTime();
+    if (!at && !bt) return 0;
+    if (!at) return 1;
+    if (!bt) return -1;
+    if (at < bt) return 1 * factor;
+    if (at > bt) return -1 * factor;
+    return 0;
+  });
+}
---
+++ content/2025/0526-republica.md
@@ -0,0 +1,15 @@
+---
+title: "Re:publica"
+description:
+  "I will be speaking at Re:publica. Details to follow."
+date: 2025-05-26
+location: Berlin, DE
+tags:
+  - news
+  - sovereignty
+  - conference
+---
+
+tk
+- pic of the conference
+- short abstract
